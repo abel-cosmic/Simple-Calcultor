805a9c69f23774aa0d830dd4c61a7f0adcb26e3f
abel-cosmic <abelshibabaw@heuc.edu.et>
2023-04-25 00:51:31 +0300

    final commit!!
    corrected every calculations and the displaying techniques and
    removed the Calculator class because it was confusing me and i don't see the neccesity for the class.

--- app/src/main/java/com/example/simplecalculator/MainActivity.java
+++ app/src/main/java/com/example/simplecalculator/MainActivity.java
@@ -1,190 +1,143 @@
 package com.example.simplecalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
-/*
-* TODO: use one of the following layouts:
-*  - LinearLayout
-*  - AbsoluteLayout
-*  - RelativeLayout
-*  - FrameLayout
-*  - ScrollView
-*
-* */
-/*
-* TODO: we have to do the following functionalities
-*  - addition
-*  - substraction
-*  - division
-*  - multiplication
-*  - clear
-*  - all clear
-*
-* */
-/*
-* TODO: the button names should be the name of the numbers like: 1 == one....
-*  numbered Buttons : 1,2,3,4,5,6,7,8,9,0
-*  decimal button : .
-*  operation buttons : +, - , / , *
-*  extra button : CLEAR, All CLEAR
-* */
+import java.text.BreakIterator;
+
 public class MainActivity extends AppCompatActivity {
-    private Calculator calculator;
-    private String temp;
-    private int number,result;
+    private double num1 = 0;
+    private double num2 = 0;
+    private String operator = "";
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) throws Resources.NotFoundException {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         //create the object of calculator
         Button[] digitButtons;
-        Button[] operationButtons;
-
-        calculator = new Calculator();
-
-        // Initialize digit buttons
-        digitButtons = new Button[10];
-        digitButtons[0] =  findViewById(R.id.zero);
-        digitButtons[1] =  findViewById(R.id.one);
-        digitButtons[2] =  findViewById(R.id.two);
-        digitButtons[3] = findViewById(R.id.three);
-        digitButtons[4] = findViewById(R.id.four);
-        digitButtons[5] = findViewById(R.id.five);
-        digitButtons[6] = findViewById(R.id.six);
-        digitButtons[7] = findViewById(R.id.seven);
-        digitButtons[8] = findViewById(R.id.eight);
-        digitButtons[9] = findViewById(R.id.nine);
-
-
-
-         // add an onClick listener for each digitButton
-        for (int i = 0; i < digitButtons.length; i++) {
-            int digit = i;
-            digitButtons[i].setOnClickListener(
+        Button[] buttons = new Button[] {
+                findViewById(R.id.zero),
+                findViewById(R.id.one),
+                findViewById(R.id.two),
+                findViewById(R.id.three),
+                findViewById(R.id.four),
+                findViewById(R.id.five),
+                findViewById(R.id.six),
+                findViewById(R.id.seven),
+                findViewById(R.id.eight),
+                findViewById(R.id.nine),
+                findViewById(R.id.adder),
+                findViewById(R.id.minus),
+                findViewById(R.id.multiply),
+                findViewById(R.id.divide),
+                findViewById(R.id.AC),
+                findViewById(R.id.cancel),
+                findViewById(R.id.dot),
+                findViewById(R.id.equals)
+        };
+        for (Button button : buttons) {
+            button.setOnClickListener(
                     new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    calculator.enterDigit(digit);
-                    EditText equation=(EditText) findViewById(R.id.equation);
-                    temp = String.valueOf(equation.getText());
-                    equation.setText(temp + digit);
-                    calculator.updateDisplay();
-                }
-            });
+                        @Override
+                        public void onClick(View v) {
+                            EditText equationEditText = (EditText) findViewById(R.id.equation);
+                            EditText resultEditText = (EditText) findViewById(R.id.answer);
+                            performClick(v, equationEditText, resultEditText);
+                        }
+                    });
         }
 
-        // Initialize operation buttons
 
-        operationButtons = new Button[5];
-        operationButtons[0] = findViewById(R.id.adder);
-        operationButtons[1] = findViewById(R.id.minus);
-        operationButtons[2] = findViewById(R.id.divide);
-        operationButtons[3] = findViewById(R.id.multiply);
-        operationButtons[4] = findViewById(R.id.equals);
 
-        // Set onClickListener for operation buttons
+    }
+    private void performClick(View view, EditText equationEditText, EditText resultEditText) {
+        Button button = (Button) view;
+        String text = button.getText().toString();
 
-        for (int i = 0; i < operationButtons.length; i++) {
-            final String operationSymbol;
-            switch (i) {
-                case 0:
-                    operationSymbol = "+";
-                    break;
-                case 1:
-                    operationSymbol = "-";
-                    break;
-                case 2:
-                    operationSymbol = "/";
-                    break;
-                case 3:
-                    operationSymbol = "x";
-                    break;
-                case 4:
-                    operationSymbol = "=";
-                    break;
-                default:
-                    operationSymbol = "";
-                    break;
+        if (text.matches("[0-9.]")) {
+
+            // Handle digit and decimal point button click event
+
+            String equation = equationEditText.getText().toString();
+            equationEditText.setText(equation + text);
+            resultEditText.setText(""); // Clear result
+
+        } else if (text.matches("[+\\-*/]")) {
+
+            // Handle operator button click event
+
+            if (!equationEditText.getText().toString().isEmpty()) {
+                num1 = Double.parseDouble(equationEditText.getText().toString());
+                operator = text;
+                equationEditText.setText(""); // Clear expression
+                resultEditText.setText(num1 + " " + operator + " ");//show equation with out the result and equals sign.
+
             }
-            operationButtons[i].setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    calculator.performOperation(operationSymbol);
-                    calculator.updateDisplay();
-                    EditText answer = (EditText) findViewById(R.id.answer);
-                    answer.setText(String.valueOf(calculator.getResult()));
-                    EditText equation=(EditText) findViewById(R.id.equation);
-                    temp = String.valueOf(equation.getText());
-                    if(calculator.getResult() == 0.0){
-                        answer.setText(calculator.getValue());
-                    }
-                    if(operationSymbol == "="){
-                        equation.setText(String.valueOf(calculator.getResult()));
-                        answer.setText(" ");
-                    }else{
-                        equation.setText(temp + operationSymbol);
-                    }
 
-                }
-            });
+        } else if (text.equals("=")) {
+
+            // Handle equals button click event
+
+            if (!(operator.isEmpty())) {
+
+                num2 = Double.parseDouble(equationEditText.getText().toString());
+                double result = performOperation(num1, num2, operator);
+                resultEditText.setText(num1 + " " + operator + " " + num2 + " = " + result); // Show full equation with result
+                equationEditText.setText(Double.toString(result)); // Show result only
+                num1 = result; // Store result as num1 for subsequent calculations
+                operator = "";
+            }
+        } else if (text.equals("C")) {
+
+            // Handle clear button click event
+
+            String equation = equationEditText.getText().toString();
+            if (!equation.isEmpty()) {
+                equationEditText.setText(equation.substring(0, equation.length() - 1));
+            }
         }
-        //for displaying the equation portion only
+        else if (text.equals("AC")) {
 
-        Button btnAc = (Button)findViewById(R.id.AC);
-        Button btnC=(Button)findViewById(R.id.cancel);
-        Button btnDot=(Button) findViewById(R.id.dot);
-        // Initialize extras buttons
-        btnDot.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        calculator.enterDecimal();
-                        calculator.updateDisplay();
-                        EditText displayView = (EditText) findViewById(R.id.answer);
-                        if(calculator.getResult() == 0.0){
-                            displayView.setText(calculator.getValue());
-                        }
-                        displayView.setText(String.valueOf(calculator.getResult()));
-                    }
-                }
-        );
-        btnAc.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        calculator.clearAll();
-                        calculator.updateDisplay();
-                        EditText equation = (EditText) findViewById(R.id.equation);
-                        EditText answer = (EditText) findViewById(R.id.answer);
-                        answer.setText(" ");
-                        equation.setText(" ");
-                    }
-                }
-        );
-        btnC.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        calculator.clearEntry();
-                        calculator.updateDisplay();
-                        EditText equation = (EditText) findViewById(R.id.equation);
-                        EditText answer = (EditText) findViewById(R.id.answer);
-                        if(calculator.getResult() == 0.0){
-                            equation.setText(" ");
-                            answer.setText(String.valueOf(calculator.getResult()));
-                        }else{
-                            equation.setText(String.valueOf(calculator.getResult()));
-                        }
+            // Handle all clear button click event
 
-                    }
+            num1 = 0;
+            num2 = 0;
+            operator = "";
+            equationEditText.setText("");
+            resultEditText.setText("");
+        }
+        else if (text.equals(".")) {
+            // Handle decimal point button click event
+            String equation = equationEditText.getText().toString();
+            if (equation.isEmpty()) {
+                equationEditText.setText("0.");
+            } else if (!equation.contains(".")) {
+                equationEditText.setText(equation + ".");
+            }
+        }
+    }
+    private double performOperation(double num1, double num2, String operator) {
+        switch (operator) {
+            case "+":
+                return num1 + num2;
+            case "-":
+                return num1 - num2;
+            case "*":
+                return num1 * num2;
+            case "/":
+                if (num2 != 0) {
+                    return num1 / num2;
+                } else {
+                    return Double.NaN;
                 }
-        );
+            default:
+                return 0.0;
+        }
     }
 }
